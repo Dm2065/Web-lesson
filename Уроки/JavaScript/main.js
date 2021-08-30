c7569879da8c1234ebdef265947418964d9b846d
Dm2065 <fat2065@gmail.com>
2021-08-30 12:51:51 +0700

    Добавлены файлы JS

---
+++ Уроки/JavaScript/main.js
@@ -0,0 +1,11 @@
+let a= new Date(2015,1,28)
+a.setDate(a.getDate() + 23)
+console.log(a)
+
+
+/**
+ * Функция преобразует строку в дату
+ * @param {string} time строка
+ */
+function timeTiMinute(time){
+}
